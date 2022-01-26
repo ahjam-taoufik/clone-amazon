4b1abf07a1f3588bf65dd66ce5f1aa232c99e624
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-26 17:37:49 +0100

    Home and Product Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
-
+import Home from './Home/Home'
 import Header from './header/Header';
 
 function App() {
   return (
     <div className="App">
         <Header/>
+        <Home/>
     </div>
   );
 }
---
+++ src/Home/Home.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Home.css";
+import Product from "./../Product/Product";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+          //   src="https://m.media-amazon.com/images/I/61jovjd+f9L._SX3000_.jpg"
+          alt=""
+        />
+            <div className="home__row">
+            <Product
+                id='123456'
+                title="this is Tittle of product one"
+                price={12.14}
+                rating={4}
+                image="https://www.usinenouvelle.com/expo/img/mini-pc-industriel-embarque-durci-et-economique-nuvo-7501-series-011779725-product_zoom.jpg"
+            />
+            <Product />
+            <Product />
+            
+            </div>
+            <div className="home__row">
+            <Product
+                id='123456'
+                title="this is Tittle of product one"
+                price={12.14}
+                rating={4}
+                image="https://www.usinenouvelle.com/expo/img/mini-pc-industriel-embarque-durci-et-economique-nuvo-7501-series-011779725-product_zoom.jpg"
+            />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            
+            </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
